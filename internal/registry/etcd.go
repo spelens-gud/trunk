96bd96d0eabc2c14fa051cd60ccbbdae9f505560
xt <1014797089@qq.com>
2025-11-11 15:05:53 +0800

    feat(registry): 更新etcd注册服务缓存模块

--- internal/registry/etcd.go
+++ internal/registry/etcd.go
@@ -34,80 +34,43 @@ func (s *ServiceCache) New() {
 	s.key = s.cnf.Key
 }
 
-func (s *ServiceCache) Publisher(value string) error {
+// Publisher 发布服务
+func (s *ServiceCache) Publisher(value string) {
 
 	s.val = value
 
-	if err := s.putKeyWithLease(6); err != nil {
-		return fmt.Errorf(" server put key err:%s", err)
-	}
-
-	return nil
+	// 错误处理全在调用链最底层, 不讲错误外抛
+	s.putKeyWithLease(6)
 }
 
 func (s *ServiceCache) GetCacheClient() *clientv3.Client {
 	return s.cli
 }
 
-func (s *ServiceCache) Put(ctx context.Context, key string, val string) error {
+// Put 创建服务
+func (s *ServiceCache) Put(ctx context.Context, key string, val string) {
 	s.log.Infof("put key:%s val:%s", key, val)
-	_, err := s.cli.Put(ctx, key, val)
-
-	return err
+	assert.ShouldValue(s.cli.Put(ctx, key, val))
 }
 
 // GetValue 获取值
-func (s *ServiceCache) GetValue(key string, opts ...clientv3.OpOption) (string, error) {
-	resp, err := s.cli.Get(context.Background(), key, opts...)
-	if err != nil {
-		return "", err
-	}
+func (s *ServiceCache) GetValue(key string, opts ...clientv3.OpOption) string {
+	resp := assert.ShouldValue(s.cli.Get(context.Background(), key, opts...))
 	if len(resp.Kvs) == 0 {
-		return "", nil
+		return ""
 	}
 
-	return string(resp.Kvs[0].Value), nil
+	return string(resp.Kvs[0].Value)
 }
 
 // GetValues 获取值
-func (s *ServiceCache) GetValues(key string, opts ...clientv3.OpOption) ([]*mvccpb.KeyValue, error) {
-	resp, err := s.cli.Get(context.Background(), key, opts...)
-	if err != nil {
-		return nil, err
-	}
+func (s *ServiceCache) GetValues(key string, opts ...clientv3.OpOption) []*mvccpb.KeyValue {
+	resp := assert.ShouldValue(s.cli.Get(context.Background(), key, opts...))
 
-	return resp.Kvs, nil
+	return resp.Kvs
 }
 
-// 设置租约
-func (s *ServiceCache) putKeyWithLease(lease int64) error {
-	// 创建一个新的租约，并设置ttl时间
-	resp, err := s.cli.Grant(context.Background(), lease)
-	if err != nil {
-		return err
-	}
-
-	// 注册服务并绑定租约
-	_, err = s.cli.Put(context.Background(), fmt.Sprintf("%s/%d", s.key, resp.ID), s.val, clientv3.WithLease(resp.ID))
-	if err != nil {
-		return err
-	}
-	// 设置续租 定期发送需求请求
-	// KeepAlive使给定的租约永远有效。 如果发布到通道的keepalive响应没有立即被使用，
-	// 则租约客户端将至少每秒钟继续向etcd服务器发送保持活动请求，直到获取最新的响应为止。
-	// registry client会自动发送ttl到etcd server，从而保证该租约一直有效
-	leaseRespChan, err := s.cli.KeepAlive(context.Background(), resp.ID)
-	if err != nil {
-		return err
-	}
-
-	s.leaseID = resp.ID
-	s.keepAliveChan = leaseRespChan
-	s.log.Infof("Put key:%s  val:%s  success!", s.key, s.val)
-
-	return nil
-}
-
+// GetLeaseID 获取租约ID
 func (s *ServiceCache) GetLeaseID() uint64 {
 	return uint64(s.leaseID)
 }
@@ -124,20 +87,33 @@ func (s *ServiceCache) ListenLeaseRespChan() {
 }
 
 // Close 注销服务
-func (s *ServiceCache) Close() error {
+func (s *ServiceCache) Close() {
 	s.log.Infof("close service cache...")
 	if s.cli == nil {
-		return nil
+		s.log.Errorf("etcd v3客户端为空")
 	}
 	// 撤销租约
-	if _, err := s.cli.Revoke(context.Background(), s.leaseID); err != nil {
-		return err
-	}
+	assert.ShouldValue(s.cli.Revoke(context.Background(), s.leaseID))
 	s.log.Infof("撤销租约")
-	if err := s.cli.Close(); err != nil {
-		return err
-	}
-	s.log.Infof("ServiceCache[%d] close success!", s.leaseID)
 
-	return nil
+	assert.ShouldFunc(s.cli.Close)
+	s.log.Infof("ServiceCache[%d] close success!", s.leaseID)
+}
+
+// putKeyWithLease 设置租约
+func (s *ServiceCache) putKeyWithLease(lease int64) {
+	// 创建一个新的租约，并设置ttl时间
+	resp := assert.ShouldValue(s.cli.Grant(context.Background(), lease))
+
+	// 注册服务并绑定租约
+	assert.ShouldValue(s.cli.Put(context.Background(), fmt.Sprintf("%s/%d", s.key, resp.ID), s.val, clientv3.WithLease(resp.ID)))
+	// 设置续租 定期发送需求请求
+	// KeepAlive使给定的租约永远有效。 如果发布到通道的keepalive响应没有立即被使用，
+	// 则租约客户端将至少每秒钟继续向etcd服务器发送保持活动请求，直到获取最新的响应为止。
+	// registry client会自动发送ttl到etcd server，从而保证该租约一直有效
+	leaseRespChan := assert.ShouldValue(s.cli.KeepAlive(context.Background(), resp.ID))
+
+	s.leaseID = resp.ID
+	s.keepAliveChan = leaseRespChan
+	s.log.Infof("Put key:%s  val:%s  success!", s.key, s.val)
 }
