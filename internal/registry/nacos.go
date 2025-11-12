62f76ef2c75b0b239199b67d43b84c891c8a1f32
xt <1014797089@qq.com>
2025-11-12 20:00:32 +0800

    feat(consul): 实现nacos的registry的功能

--- internal/registry/nacos.go
+++ internal/registry/nacos.go
@@ -2,10 +2,12 @@ package registry
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/nacos-group/nacos-sdk-go/v2/clients"
 	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
 	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/model"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 
 	"sync"
@@ -29,7 +31,6 @@ var _ Registry = (*NacosRegistry)(nil)
 // New 初始化nacos客户端
 func (n *NacosRegistry) New() {
 	n.ctx, n.cancel = context.WithCancel(context.Background())
-	n.log.Infof("初始化nacos注册中心，服务器: %v", n.cnf.Hosts)
 
 	serverConfigs := make([]constant.ServerConfig, 0, len(n.cnf.Hosts))
 	for _, host := range n.cnf.Hosts {
@@ -66,7 +67,7 @@ func (n *NacosRegistry) New() {
 
 	n.namingClient = namingClient
 
-	n.log.Warnf("nacos注册中心实现待完成，请引入 github.com/nacos-group/nacos-sdk-go/v2")
+	n.log.Infof("初始化nacos注册中心，服务器: %v", n.cnf.Hosts)
 }
 
 // Publisher 注册服务
@@ -76,7 +77,6 @@ func (n *NacosRegistry) Publisher(value string) {
 
 	n.log.Infof("注册nacos服务: %s", n.cnf.ServiceName)
 
-	// TODO: 实现服务注册
 	success, err := n.namingClient.RegisterInstance(vo.RegisterInstanceParam{
 		Ip:          n.cnf.IP,
 		Port:        n.cnf.ServicePort,
@@ -89,6 +89,7 @@ func (n *NacosRegistry) Publisher(value string) {
 		Ephemeral:   n.cnf.IsEphemeral(),
 		Metadata:    n.cnf.Metadata,
 	})
+
 	if err != nil || !success {
 		n.log.Errorf("注册nacos服务失败: %v", err)
 		return
@@ -125,22 +126,16 @@ func (n *NacosRegistry) GetValue(key string, opts ...interface{}) string {
 	n.lock.RLock()
 	defer n.lock.RUnlock()
 
-	// TODO: 实现获取服务实例
-	// 示例代码:
-	/*
-		instance, err := n.namingClient.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
-			ServiceName: key,
-			GroupName:   n.cnf.GetGroupName(),
-			Clusters:    []string{n.cnf.GetClusterName()},
-		})
-		if err != nil {
-			n.log.Errorf("获取nacos服务实例失败: %v", err)
-			return ""
-		}
-		return fmt.Sprintf("%s:%d", instance.Ip, instance.Port)
-	*/
-
-	return ""
+	instance, err := n.namingClient.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
+		ServiceName: key,
+		GroupName:   n.cnf.GetGroupName(),
+		Clusters:    []string{n.cnf.GetClusterName()},
+	})
+	if err != nil {
+		n.log.Errorf("获取nacos服务实例失败: %v", err)
+		return ""
+	}
+	return fmt.Sprintf("%s:%d", instance.Ip, instance.Port)
 }
 
 // GetValues 获取所有服务实例
@@ -148,23 +143,17 @@ func (n *NacosRegistry) GetValues(key string, opts ...interface{}) interface{} {
 	n.lock.RLock()
 	defer n.lock.RUnlock()
 
-	// TODO: 实现获取所有服务实例
-	// 示例代码:
-	/*
-		instances, err := n.namingClient.SelectInstances(vo.SelectInstancesParam{
-			ServiceName: key,
-			GroupName:   n.cnf.GetGroupName(),
-			Clusters:    []string{n.cnf.GetClusterName()},
-			HealthyOnly: true,
-		})
-		if err != nil {
-			n.log.Errorf("获取nacos服务实例列表失败: %v", err)
-			return nil
-		}
-		return instances
-	*/
-
-	return nil
+	instances, err := n.namingClient.SelectInstances(vo.SelectInstancesParam{
+		ServiceName: key,
+		GroupName:   n.cnf.GetGroupName(),
+		Clusters:    []string{n.cnf.GetClusterName()},
+		HealthyOnly: true,
+	})
+	if err != nil {
+		n.log.Errorf("获取nacos服务实例列表失败: %v", err)
+		return nil
+	}
+	return instances
 }
 
 // Put 创建或更新服务
@@ -177,25 +166,21 @@ func (n *NacosRegistry) Put(ctx context.Context, key string, val string) {
 func (n *NacosRegistry) Watch(ctx context.Context, prefix string) interface{} {
 	n.log.Infof("开始监听nacos服务变化: %s", prefix)
 
-	// TODO: 实现服务监听
-	// 示例代码:
-	/*
-		err := n.namingClient.Subscribe(&vo.SubscribeParam{
-			ServiceName: prefix,
-			GroupName:   n.cnf.GetGroupName(),
-			Clusters:    []string{n.cnf.GetClusterName()},
-			SubscribeCallback: func(services []model.Instance, err error) {
-				if err != nil {
-					n.log.Errorf("nacos服务变化回调错误: %v", err)
-					return
-				}
-				n.log.Infof("nacos服务变化: %v", services)
-			},
-		})
-		if err != nil {
-			n.log.Errorf("订阅nacos服务失败: %v", err)
-		}
-	*/
+	err := n.namingClient.Subscribe(&vo.SubscribeParam{
+		ServiceName: prefix,
+		GroupName:   n.cnf.GetGroupName(),
+		Clusters:    []string{n.cnf.GetClusterName()},
+		SubscribeCallback: func(services []model.Instance, err error) {
+			if err != nil {
+				n.log.Errorf("nacos服务变化回调错误: %v", err)
+				return
+			}
+			n.log.Infof("nacos服务变化: %v", services)
+		},
+	})
+	if err != nil {
+		n.log.Errorf("订阅nacos服务失败: %v", err)
+	}
 
 	return nil
 }
